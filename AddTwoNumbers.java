351017c8baa9ffcafbfe69f31da003d11e6e2639
p-i-k-l-u <piklu19jan@gmail.com>
2024-08-15 19:49:22 +0530

    first commit

---
+++ AddTwoNumbers.java
@@ -0,0 +1,66 @@
+class AddTwoNumbers {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        int carry = 0;
+
+        while (l1 != null || l2 != null) {
+            int sum = carry;
+
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            carry = sum / 10;
+            sum = sum % 10;
+            curr.next = new ListNode(sum);
+            curr = curr.next;
+        }
+
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        // Creating two linked lists
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        // Adding the two numbers
+        AddTwoNumbers solution = new AddTwoNumbers();
+        ListNode result = solution.addTwoNumbers(l1, l2);
+
+        // Printing the result
+        while (result != null) {
+            System.out.print(result.val);
+            if (result.next != null) {
+                System.out.print(" -> ");
+            }
+            result = result.next;
+        }
+    }
+}
