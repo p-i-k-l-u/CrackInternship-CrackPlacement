445e398e9baf05bf82c2e8dee5a7ad41e2c2d086
p-i-k-l-u <piklu19jan@gmail.com>
2024-08-27 00:25:46 +0530

    first commit

---
+++ TCS_NQT/DecimalToBinary.java
@@ -0,0 +1,20 @@
+package TCS_NQT;
+
+public class DecimalToBinary {
+
+    public static void main(String[] args) {
+        int n = 28;
+        int[] binary = new int[32];
+        // int pow = 1;
+        int i = 0;
+        while (n > 0) {
+            binary[i] = n % 2;
+            i++;
+            n /= 2;
+
+        }
+        for (int ind = i - 1; ind >= 0; ind--) {
+            System.out.print(binary[ind]);
+        }
+    }
+}
