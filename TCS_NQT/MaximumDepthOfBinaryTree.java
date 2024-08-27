a472846a618bf15b89b9e34d8b69f5c55ed31815
p-i-k-l-u <piklu19jan@gmail.com>
2024-08-28 00:37:45 +0530

    first commit

---
+++ TCS_NQT/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,46 @@
+package TCS_NQT;
+
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class MaximumDepthOfBinaryTree {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        int lh = maxDepth(root.left);
+        int rh = maxDepth(root.right);
+
+        return 1 + Math.max(lh, rh);
+    }
+
+    public static void main(String[] args) {
+        MaximumDepthOfBinaryTree mdbt = new MaximumDepthOfBinaryTree();
+
+        // Creating a sample tree:
+        // 1
+        // / \
+        // 2 3
+        // / \ \
+        // 4 5 6
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.right = new TreeNode(6);
+
+        int result = mdbt.maxDepth(root);
+        System.out.println("Maximum Depth of the Binary Tree: " + result);
+    }
+}
